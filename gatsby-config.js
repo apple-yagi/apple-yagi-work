b8dee4d4fc0e8fedbea3ed9b1fcea725998a3928
apple-yagi <applesryu@gmail.com>
2021-06-24 00:35:11 +0900

    fix: assets image

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,6 +51,13 @@ module.exports = {
       }
     },
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/static/assets`
+      }
+    },
+    {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
